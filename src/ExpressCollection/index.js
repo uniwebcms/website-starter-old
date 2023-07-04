f0543d7f9fdecdc40c7df8a79d042e1954ad527f
XIANG ZUO <35638284+xiangzuo951218@users.noreply.github.com>
2023-07-04 11:45:21 -0400

    init

---
+++ src/ExpressCollection/index.js
@@ -0,0 +1,6 @@
+// Import the express library
+import * as expressModules from '@uniwebcms/express'; // See README for more details: https://github.com/uniwebcms/express/blob/main/README.md
+// import CSS
+import './index.css';
+
+export default expressModules;
